de2e3cbdeb7cd1b8c103ab22004bd48c202ce048
samczhang <zhangchen2397@126.com>
2016-07-28 13:50:27 +0800

    change path

--- example/asset/demo.js
+++ example/asset/demo.js
@@ -3,18 +3,18 @@
         data: [{
             title: '曾经的你',
             author: '许巍',
-            src: '/example/asset/music/once.mp3',
-            cover: '/example/asset/image/cover_once.jpeg'
+            src: 'https://zhangchen2397.github.io/audioPlayer/example/asset/music/once.mp3',
+            cover: 'https://zhangchen2397.github.io/audioPlayer/example/asset/image/cover_once.jpeg'
         }, {
             title: 'You\'re Beautiful',
             author: 'James Blunt',
-            src: '/example/asset/music/you_are_beautiful.mp4',
-            cover: '/example/asset/image/cover_yab.jpg'
+            src: 'https://zhangchen2397.github.io/audioPlayer/example/asset/music/you_are_beautiful.mp4',
+            cover: 'https://zhangchen2397.github.io/audioPlayer/example/asset/image/cover_yab.jpg'
         }, {
             title: 'I\'m Yours',
             author: 'Jason Mraz',
-            src: '/example/asset/music/i_am_yours.mp4',
-            cover: '/example/asset/image/cover_iay.jpg'
+            src: 'https://zhangchen2397.github.io/audioPlayer/example/asset/music/i_am_yours.mp4',
+            cover: 'https://zhangchen2397.github.io/audioPlayer/example/asset/image/cover_iay.jpg'
         }]
     });
 })();
